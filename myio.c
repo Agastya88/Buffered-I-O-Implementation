811604ac1fecde494e475aa48dba2309797d4fff
aahluwalia <aahluwalia@middlebury.edu>
2019-11-14 21:45:50 -0500

    cleaned up comments and indentation

--- myio.c
+++ myio.c
@@ -38,22 +38,21 @@ struct file *myopen(const char *pathname, const char *mode){
   fileOpened->bytesInWriteBuffer = 0;
   fileOpened->fD = fileDesc;
   return fileOpened;
-  //still need to implement some stuff like
-  //the file creation flags specified - used in
-  //exceptional situations like when there is an error
 }
 
 int myflush(struct file *stream){
   size_t flushingBytesWritten;
+  //checking if their is a need to flush
   if (stream->bytesInWriteBuffer!=0){
     if (stream->bytesInWriteBuffer<=BUFFER_SIZE){
      flushingBytesWritten
      = write (stream->fD, stream->writeBuffer ,stream->bytesInWriteBuffer);
     }
   }
+  //clearing the write buffer once flushing is completed
   stream->bytesInWriteBuffer = 0;
   memset (stream->writeBuffer, 0, sizeof (stream->writeBuffer));
-  //clearing buffer
+  //error checking
   if (flushingBytesWritten==-1){
    return EOF;
   }
@@ -75,8 +74,6 @@ size_t mywrite(const char *buf, size_t nmemb, struct file* stream){
     /*creates a buffer so that the sys call write is only
     called when "enough bytes" accumulate*/
     size_t noOfBytesWritten=0;
-    /*the code below is basically adding the text the user wants to write
-    to the buffer of text also keeping track of that number of bytes*/
     int bufferLength = stream->bytesInWriteBuffer+nmemb;
     //when it doesn't send the number of bytes over bufferSize
     if (bufferLength<BUFFER_SIZE){
@@ -89,15 +86,15 @@ size_t mywrite(const char *buf, size_t nmemb, struct file* stream){
     else{
     //when the new my write call sends the bytes over bufferSize
       while (bufferLength>=BUFFER_SIZE){
-          //add bytes till buffer fills
+        //add bytes till buffer fills
         for (int i=0; i<BUFFER_SIZE-(stream->bytesInWriteBuffer);i++){
         stream->writeBuffer[(stream->bytesInWriteBuffer)+i] = *(buf+i);
         noOfBytesWritten++;
         }
         write (stream->fD, stream->writeBuffer, BUFFER_SIZE);
+        //clearing the buffer
         stream->bytesInWriteBuffer=0;
         memset (stream->writeBuffer, 0, sizeof (stream->writeBuffer));
-        //above two lines are clearing the buffer
         bufferLength = bufferLength - BUFFER_SIZE;
         //bufferLength is now the updated number of bytes to be written
         //in a way it is the excess bytes from "buf" that didn't get written
@@ -121,116 +118,114 @@ size_t mywrite(const char *buf, size_t nmemb, struct file* stream){
 }
 
 size_t myread(char *ptr, size_t nmemb, struct file *stream){
-    //myRead front loads the operation, unlike myWrite; what this
-    //essentially means is that with the first call to myread
-    //we read for the full buffer size; and then we keep track
-    //of it according to the manner in which myRead is called
-    int noOfBytesRead = 0;
-    //incase file is both read and write, we call flush
-    //before starting the read on the file
-    int flushResult = myflush (stream);
-    if (flushResult != 0){
-      return 0;
-    }
-    //if there is no front loaded read call
-    if (stream->positionInReadBuffer == 0){
-     if (nmemb <= BUFFER_SIZE){
-         size_t sysCallReturnValue = read (stream->fD, stream->readBuffer, BUFFER_SIZE);
-         if (sysCallReturnValue == -1){
-           return 0;
-         }
-         if (sysCallReturnValue != 0){
-             for (int i=0; i<nmemb;i++){
-                 if (stream->readBuffer[i] != '\0'){
-                   *(ptr+i) = stream->readBuffer [i];
-                   stream->positionInReadBuffer = i+1;
-                   noOfBytesRead++;
-                 }
-             }
-          }
-       }
-     while (nmemb>BUFFER_SIZE){
-         size_t sysCallReturnValue = read (stream->fD, stream->readBuffer, BUFFER_SIZE);
-         if (sysCallReturnValue == -1){
-           return 0;
-         }
-         if (sysCallReturnValue != 0){
-             for (int i=0;i<BUFFER_SIZE; i++){
-               if (stream->readBuffer [stream->positionInReadBuffer] != '\0'){
-                   *(ptr+i) = stream->readBuffer [stream->positionInReadBuffer];
-                   stream->positionInReadBuffer++;
-                   noOfBytesRead++;
-               }
-             }
-             continue;
-         }
-         nmemb = nmemb - BUFFER_SIZE;
-         if (nmemb <= BUFFER_SIZE){
-             size_t sysCallReturnValue = read (stream->fD, stream->readBuffer, BUFFER_SIZE);
-             if (sysCallReturnValue == -1){
-               return 0;
-             }
-             if (sysCallReturnValue !=0){
-                 for (int i=0; i<nmemb;i++){
-                   if (stream->readBuffer [i] != '\0'){
-                     *(ptr+i) = stream->readBuffer [i];
-                     stream->positionInReadBuffer = i+1;
-                     noOfBytesRead++;
-                   }
-                 }
-             }
-         }
-     }
-    }
-    //if there is a front loaded read call
-    else{
-     int bytesThatCanBeReadWithoutSysCall = BUFFER_SIZE - stream->positionInReadBuffer;
-     if (bytesThatCanBeReadWithoutSysCall>=nmemb){
-     //then simply just give them what they want
-      for (int j=0; j<nmemb; stream->positionInReadBuffer++, j++){
-      *(ptr+j) = stream->readBuffer [stream->positionInReadBuffer];
-       noOfBytesRead++;
-       }
-      }
-     else if (bytesThatCanBeReadWithoutSysCall<nmemb){
-     //now you cannot just give them what they want, because you
-     //don't have it, so you need a sys call
-     //first give them the ones that are already in the buffer
-      for (int j=0; j<bytesThatCanBeReadWithoutSysCall; stream->positionInReadBuffer++, j++){
-      *(ptr+j) = stream->readBuffer [stream->positionInReadBuffer];
+ /*myRead front loads the operation, unlike myWrite; what this
+ essentially means is that with the first call to myread
+ we read for the full buffer size; and then we keep track
+ of it according to the manner in which myRead is called*/
+ int noOfBytesRead = 0;
+ //incase file is rw, we call flush before reading
+ int flushResult = myflush (stream);
+ if (flushResult != 0){
+   return 0;
+ }
+ //if there is no front loaded read call
+ if (stream->positionInReadBuffer == 0){
+  if (nmemb <= BUFFER_SIZE){
+   size_t sysCallReturnValue = read (stream->fD, stream->readBuffer, BUFFER_SIZE);
+   if (sysCallReturnValue == -1){
+    return 0;
+   }
+   if (sysCallReturnValue != 0){
+    for (int i=0; i<nmemb;i++){
+     if (stream->readBuffer[i] != '\0'){
+      *(ptr+i) = stream->readBuffer [i];
+      stream->positionInReadBuffer = i+1;
       noOfBytesRead++;
       }
-      //then give them the ones that require sys calls
-      int noOfBytesThatStillNeedToBeRead = nmemb - bytesThatCanBeReadWithoutSysCall;
-      stream->positionInReadBuffer = 0;
-      int updateValForPtrLocation = bytesThatCanBeReadWithoutSysCall;
-      if (noOfBytesThatStillNeedToBeRead <= BUFFER_SIZE){
-          size_t sysCallReturnValue = read (stream->fD, stream->readBuffer, BUFFER_SIZE);
-          if (sysCallReturnValue == -1){
-            return 0;
-          }
-          for (int j=updateValForPtrLocation;
-            stream->positionInReadBuffer<noOfBytesThatStillNeedToBeRead;
-            stream->positionInReadBuffer++, j++){
-            *(ptr+j) = stream->readBuffer [stream->positionInReadBuffer];
-            noOfBytesRead++;
-          }
+     }
+    }
+   }
+  while (nmemb>BUFFER_SIZE){
+   size_t sysCallReturnValue = read (stream->fD, stream->readBuffer, BUFFER_SIZE);
+   if (sysCallReturnValue == -1){
+    return 0;
+   }
+   if (sysCallReturnValue != 0){
+    for (int i=0;i<BUFFER_SIZE; i++){
+     if (stream->readBuffer [stream->positionInReadBuffer] != '\0'){
+      *(ptr+i) = stream->readBuffer [stream->positionInReadBuffer];
+      stream->positionInReadBuffer++;
+      noOfBytesRead++;
       }
-      else{
-       //here I can just give them what they want directly use the system
-       //call since they want more than the buffer_size, thus making it
-       //more efficient
-       ptr = ptr+updateValForPtrLocation*(sizeof(char));
-       size_t sysCallReturnValue = read (stream->fD, ptr, noOfBytesThatStillNeedToBeRead);
-       noOfBytesRead += noOfBytesThatStillNeedToBeRead;
-       if (sysCallReturnValue == -1){
-         return 0;
-       }
-       stream->positionInReadBuffer = 0;
+     }
+    continue;
+   }
+   nmemb = nmemb - BUFFER_SIZE;
+   if (nmemb <= BUFFER_SIZE){
+    size_t sysCallReturnValue = read (stream->fD, stream->readBuffer, BUFFER_SIZE);
+    if (sysCallReturnValue == -1){
+     return 0;
+    }
+    if (sysCallReturnValue !=0){
+     for (int i=0; i<nmemb;i++){
+      if (stream->readBuffer [i] != '\0'){
+       *(ptr+i) = stream->readBuffer [i];
+       stream->positionInReadBuffer = i+1;
+       noOfBytesRead++;
       }
      }
     }
-    return noOfBytesRead;
+   }
+  }
+ }
+ //if there is a front loaded read call
+ else{
+  int bytesThatCanBeReadWithoutSysCall = BUFFER_SIZE - stream->positionInReadBuffer;
+  if (bytesThatCanBeReadWithoutSysCall>=nmemb){
+   //then simply just give them what they want
+   for (int j=0; j<nmemb; stream->positionInReadBuffer++, j++){
+    *(ptr+j) = stream->readBuffer [stream->positionInReadBuffer];
+    noOfBytesRead++;
+   }
+  }
+  else if (bytesThatCanBeReadWithoutSysCall<nmemb){
+   //now you cannot just give them what they want, because you
+   //don't have it, so you need a system call
+   //first give them the ones that are already in the buffer
+   for (int j=0; j<bytesThatCanBeReadWithoutSysCall; stream->positionInReadBuffer++, j++){
+   *(ptr+j) = stream->readBuffer [stream->positionInReadBuffer];
+   noOfBytesRead++;
+   }
+   //then give them the ones that require sys calls
+   int noOfBytesThatStillNeedToBeRead = nmemb - bytesThatCanBeReadWithoutSysCall;
+   stream->positionInReadBuffer = 0;
+   int updateValForPtrLocation = bytesThatCanBeReadWithoutSysCall;
+   if (noOfBytesThatStillNeedToBeRead <= BUFFER_SIZE){
+    size_t sysCallReturnValue = read (stream->fD, stream->readBuffer, BUFFER_SIZE);
+    if (sysCallReturnValue == -1){
+     return 0;
+    }
+    for (int j=updateValForPtrLocation; stream->positionInReadBuffer<noOfBytesThatStillNeedToBeRead;
+    stream->positionInReadBuffer++, j++){
+     *(ptr+j) = stream->readBuffer [stream->positionInReadBuffer];
+     noOfBytesRead++;
+    }
+   }
+   else{
+    //here I can just give them what they want directly use the system
+    //call since they want more than the buffer_size, thus making it
+    //more efficient
+    ptr = ptr+updateValForPtrLocation*(sizeof(char));
+    size_t sysCallReturnValue = read (stream->fD, ptr, noOfBytesThatStillNeedToBeRead);
+    noOfBytesRead += noOfBytesThatStillNeedToBeRead;
+    if (sysCallReturnValue == -1){
+     return 0;
+    }
+    stream->positionInReadBuffer = 0;
+   }
+  }
+ }
+ return noOfBytesRead;
 }
 
 int myseek(struct file *stream, long offset, int whence){
@@ -243,8 +238,8 @@ int myseek(struct file *stream, long offset, int whence){
    if (stream->positionInReadBuffer+offset<0){
     int rOffset = lseek (stream->fD, offset, SEEK_CUR);
     if (rOffset == -1){
-       return -1;
-     }
+     return -1;
+    }
     stream->positionInReadBuffer = 0;
     return 0;
    }
@@ -266,7 +261,7 @@ int myseek(struct file *stream, long offset, int whence){
   else if (whence == SEEK_SET){
    int rOffset = lseek (stream->fD, offset, SEEK_SET);
    if (rOffset == -1){
-     return -1;
+    return -1;
    }
    //resetting read at this position
    stream->positionInReadBuffer = 0;
